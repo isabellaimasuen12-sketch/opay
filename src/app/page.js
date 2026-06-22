a8ce4cddc7d6e9e0a0a4f43bfe7939b99c8c6233
isabellaimasuen12-sketch <isabellaimasuen12@gmail.com>
2026-06-22 17:13:50 +0100

    Add Opay dashboard UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,137 @@
-import Image from "next/image";
+"use client";
+import React,{useState,useEffect} from 'react'
+import { IoIosNotifications } from "react-icons/io";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
-export default function Home() {
+export default function Page() {
+  const [transactions,setTransactions]=useState([])
+  const fetchTransaction = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/transactions", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key":
+            "052f286de4087ef83371235a2b565f28f51ed7513bd7b1c749b80743dc0bb3d0",
+        },
+      });
+  
+      console.log("Status:", response.status);
+  
+      const data = await response.json();
+      console.log(data);
+  
+      setTransactions(data.transactions || []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(()=>{
+   fetchTransaction()
+  },[])
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="container">
+      <div className="dashboard">
+
+        <header className="header">
+          <div className="user-info">
+            <div className="initials">
+              <h1>I</h1>
+            </div>
+
+            <div className="greeting">
+              <p>Good Morning</p>
+              <h2>Welcome back, Isabella</h2>
+            </div>
+          </div>
+
+          <div className="bell">
+            <IoIosNotifications />
+          </div>
+        </header>
+
+        {/* Balance Card */}
+        <section className="balance-card">
+          <p>Available Balance</p>
+          <h1>₦0</h1>
+          <div className="account">
+          <h3>5399*******2147</h3>
+          <h2>VISA</h2>
+          </div>
+        </section>
+
+        {/* Action Buttons */}
+        <section className="actions">
+          <button className="income-btn">
+            <FaArrowDown />
+            Add Income
+          </button>
+
+          <button className="expense-btn">
+            <FaArrowUp />
+            Add Expense
+          </button>
+        </section>
+
+        {/* Statistics */}
+        <section className="stats">
+          <div className="stat-card">
+            <p>Total Income</p>
+            <h3>₦0</h3>
+          </div>
+
+          <div className="stat-card">
+            <p>Total Expense</p>
+            <h3 id='exp2'>₦0</h3>
+          </div>
+        </section>
+
+      </div>
+    
+      <section className="transactions">
+  <div className="transactions-header">
+    <h2>Recent Transactions</h2>
+    <button>See All</button>
+  </div>
+
+  <div className="transaction-list">
+    {transactions.length > 0 ? (
+      transactions.map((item) => (
+        <div key={item._id} className="transaction-card">
+          <div className="transaction-icon">
+            {item.category === "income" ? (
+              <FaArrowDown />
+            ) : (
+              <FaArrowUp />
+            )}
+          </div>
+
+          <div className="transaction-details">
+            <h4>{item.description}</h4>
+            <p>{item.category}</p>
+          </div>
+
+          <div
+            className={
+              item.category === "income"
+                ? "transaction-amount income"
+                : "transaction-amount expense"
+            }
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            {item.category === "income" ? "+" : "-"}₦
+            {item.amount.toLocaleString()}
+          </div>
         </div>
-      </main>
-    </div>
+      ))
+    ) : (
+      <div className="empty-transactions">
+        <p>No transactions yet</p>
+      </div>
+    )}
+  </div>
+</section>
+
+    </main>
   );
 }
+
